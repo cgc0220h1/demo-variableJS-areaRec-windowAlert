f385988080e6a1f9b479becc72d708d7467baece
vanduc2514 <vanduc2514@gmail.com>
2020-02-22 01:22:07 +0700

    First commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+function areaRec() {
+    let width;
+    let height;
+    let area;
+
+    width = prompt("Nhập vào chiều dài");
+    height = prompt("Nhập vào chiều rộng");
+    area = width * height;
+    alert("Diện tích hình chữ nhật là :" + area);
+}
